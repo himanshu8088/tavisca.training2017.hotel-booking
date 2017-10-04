d52b5ef92aa61dcb06c846bee921ce21ff1de6b8
Himanshu Soni <hsoni@tavisca.com>
2017-10-04 13:22:03 +0530

    added search request model binding and home controller

---
+++ src/Tavisca.Training2017.HotelBooking/Tavisca.Training2017.HotelBooking/Controllers/HotelController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tavisca.Training2017.HotelBooking.Models;
+
+namespace Tavisca.Training2017.HotelBooking.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {        
+        [HttpGet("hotel_search")]        
+        public IActionResult Search([FromQuery]SearchRQ req)
+        {
+            return Ok();
+        }        
+    }
+}
---
+++ src/Tavisca.Training2017.HotelBooking/Tavisca.Training2017.HotelBooking/Models/SearchRQ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tavisca.Training2017.HotelBooking.Models
+{
+    public class SearchRQ
+    {
+        public string SearchText { get; set; }           
+        public DateTime CheckIn { get; set; }        
+        public DateTime CheckOut { get; set; }        
+    }
+}
