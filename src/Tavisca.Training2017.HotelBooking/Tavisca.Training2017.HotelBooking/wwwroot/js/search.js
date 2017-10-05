49157279502a84bda3be72b5e6c4e5ce194c33e4
Shubhangi Bisaria <sbisaria@tavisca.com>
2017-10-05 21:31:25 +0530

    added search page support

--- src/Tavisca.Training2017.HotelBooking/Tavisca.Training2017.HotelBooking/wwwroot/js/search.js
+++ src/Tavisca.Training2017.HotelBooking/Tavisca.Training2017.HotelBooking/wwwroot/js/search.js
@@ -1,40 +1,35 @@
 ﻿
-//$(document).ready(function () {
-//    $(function () {
-//        $("#datepicker").datepicker({
-//            changeMonth: true,
-//            changeYear: true,
-//            minDate: '0m+1d',
-//            showAnim: "fadeIn",
-//            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-//        }
-//        );
-//    });
-//    $(function () {
-//        $("#tabs").tabs();
-//    });
-//    $("#sel2").on("change", function () {
-//        $("#age-box").empty();
-//        val = $("#sel2 option:selected").val();
-//        for (i = 0; i < val; i++) {
-//            $("#age-box").append(Agebox);
-//        }
-//        AgeBox = "<label>Age</label>\
-//            <div id=\"age-box\">\
-//                <select id=\"age\">\
-//                    <option value=\"1\">1</option>\
-//                    <option value=\"2\">2</option>\
-//                    <option value=\"3\">3</option>\
-//                    <option value=\"4\">4</option>\
-//                    <option value=\"5\">5</option>\
-//                    <option value=\"6\">6</option>\
-//                </select>\
-//            </div>"
-//    });
-//});
+var jsonFormatData = {};
+
+function hotelSearchRQ (searchText, checkIn, checkOut) {
+     this.data = {
+        "SearchText": searchText,
+        "CheckInDate": checkIn,
+        "CheckOutDate": checkOut
+    }  
+     jsonFormatData = this.data
+    
+};
+
+
 $("#searchClick").click(function () {
-    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-    xmlhttp.open("POST", "/json-handler");
-    xmlhttp.setRequestHeader("Content-Type", "application/json");
-    xmlhttp.send(JSON.stringify({ name: "John Rambo", time: "2pm" }));
+
+    searchPageReader();
+    var jsonData = JSON.stringify(jsonFormatData);
+   
+    $.ajax({
+        url: "http://localhost:56851/api/search",
+        type: "POST",
+        data: jsonData,
+        contentType: "application/json",
+        
+    });
 });
+
+function searchPageReader() {
+    var searchText = $('#destination').val;
+    var checkIn = $('#check-in').val;
+    var checkOut = $('#check-out').val;
+    var hotelSearchObj = new hotelSearchRQ("pune", "11/2/", "3242");
+}
+
