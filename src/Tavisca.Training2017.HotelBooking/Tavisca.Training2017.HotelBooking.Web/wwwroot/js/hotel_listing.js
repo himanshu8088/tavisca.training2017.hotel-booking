af51f664329b2eb31aea491421707c310a6ffde3
sbisaria <sbisaria@TAVDESK086>
2017-10-07 19:21:46 +0530

    added listing page handlebars template

--- src/Tavisca.Training2017.HotelBooking/Tavisca.Training2017.HotelBooking.Web/wwwroot/js/hotel_listing.js
+++ src/Tavisca.Training2017.HotelBooking/Tavisca.Training2017.HotelBooking.Web/wwwroot/js/hotel_listing.js
@@ -1,6 +1,15 @@
-﻿//$(document).ready(function () {
-//    if (sessionstorage.length > 0) {
-//        diplayhotels()
-//    }
-//})
+﻿$(function () {
 
+    var result = [{ name: "Hyatt", address: "pune", image: "https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png" },
+    { name: "Novotel", address: "Nagar Road", image: "https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png" },
+    { name: "Taj", address: "Mumbai" },
+    { name: "Bombay Hotel", address: "Guess??" }];
+
+    var template = $('#hotel-item');
+
+    var compiledTemplate = Handlebars.compile(template.html());
+
+    var html = compiledTemplate(result);
+
+    $('#hotelList-container').html(html);
+});
