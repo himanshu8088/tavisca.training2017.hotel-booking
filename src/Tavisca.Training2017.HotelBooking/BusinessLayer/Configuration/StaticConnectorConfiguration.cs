3950d7131b7c48057a62aaa14dbdc5801d7afc51
sbisaria <sbisaria@TAVDESK086>
2017-10-08 15:53:30 +0530

    Update ui and use latitude longitude from request.

--- src/Tavisca.Training2017.HotelBooking/BusinessLayer/Configuration/StaticConnectorConfiguration.cs
+++ src/Tavisca.Training2017.HotelBooking/BusinessLayer/Configuration/StaticConnectorConfiguration.cs
@@ -8,7 +8,7 @@ namespace BusinessLayer.Configuration
     {             
         public HotelsAvailConfig GetHotelsAvailConfig( DateTime checkIn, DateTime checkOut, string poi = "Pune", int passengerCount = 1, int noOfRooms = 1, float latitude = 27.173891f, float longitude = 78.042068f, int posId = 101)
         {
-            return new HotelsAvailConfig(poi,checkIn,checkOut,passengerCount = 1,noOfRooms = 1,latitude = 27.173891f,longitude = 78.042068f,posId = 101);
+            return new HotelsAvailConfig(poi, checkIn, checkOut, passengerCount = 1, noOfRooms = 1, latitude, longitude, posId = 101);
         }
 
         public RoomsAvailConfig GetRoomsAvailConfig()
