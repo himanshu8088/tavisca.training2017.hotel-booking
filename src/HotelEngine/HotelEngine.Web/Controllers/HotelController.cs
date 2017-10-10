15e48f8226b9a95f31841568d956917e10a083f1
hsoni <hsoni@TAVDESK056>
2017-10-10 22:49:27 +0530

    fix error

--- src/HotelEngine/HotelEngine.Web/Controllers/HotelController.cs
+++ src/HotelEngine/HotelEngine.Web/Controllers/HotelController.cs
@@ -4,26 +4,31 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using HotelEngine.Contracts.Contracts;
 using HotelEngine.Contracts.Models;
-using HotelEngine.Services.Factories;
+using HotelEngine.Services;
 
 namespace HotelEngine.Web.Controllers
 {
     [Route("[controller]")]
     public class HotelController : Controller
     {
+        private IHotelService _hotelService;
+        public HotelController()
+        {
+            _hotelService = new HotelService();
+        }
+
+
         [HttpPost("search")]
         public async Task<IActionResult> SearchAsync([FromBody] HotelSearchRQ searchRQ)
-        {
-            IHotelService hotelService = Factory.Get<IHotelService>() as IHotelService;
-            var hotels = await hotelService.SearchHotelsAsync(searchRQ);
+        {           
+            var hotels = await _hotelService.SearchHotelsAsync(searchRQ);
             return Ok(hotels);
         }
 
         [HttpPost("roomsearch")]
         public async Task<IActionResult> RoomSearchAsync([FromBody] RoomSearchRQ roomRQ)
-        {
-            IHotelService hotelService = Factory.Get<IHotelService>() as IHotelService;
-            var response = await hotelService.RoomSearchAsync(roomRQ);
+        {            
+            var response = await _hotelService.RoomSearchAsync(roomRQ);
             return Ok(response);
         }
     }
