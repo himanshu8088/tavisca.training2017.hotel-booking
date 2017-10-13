9c3ae66750675a5d4c41349d910e7f77d495ff04
Shubhangi Bisaria <sbisaria@tavisca.com>
2017-10-13 11:04:00 +0530

    add room image slider handlebar

--- src/HotelEngine/HotelEngine.Web/wwwroot/js/room.js
+++ src/HotelEngine/HotelEngine.Web/wwwroot/js/room.js
@@ -1,4 +1,10 @@
-﻿
+﻿Handlebars.registerHelper('ifCond', function (v1, v2, options) {
+    if (v1 != v2) {
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
+
 $(document).ready(function () {
     var searchCriteria = JSON.parse(sessionStorage.getItem('roomSearchCriteria')).data;
     var jsonData = JSON.stringify(searchCriteria);
