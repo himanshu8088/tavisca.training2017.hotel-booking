a85bb01f86beb762d3b440160b49d088c6a48c37
hsoni <hsoni@TAVDESK056>
2017-10-26 14:45:07 +0530

    add exception handling support in controller

--- src/HotelEngine/HotelEngine.Web/Controllers/HotelController.cs
+++ src/HotelEngine/HotelEngine.Web/Controllers/HotelController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using HotelEngine.Contracts.Contracts;
 using HotelEngine.Contracts.Models;
 using HotelEngine.Services;
+using System;
 
 namespace HotelEngine.Web.Controllers
 {
@@ -17,29 +18,63 @@ namespace HotelEngine.Web.Controllers
 
         [HttpPost("search")]
         public async Task<IActionResult> SearchHotelsAsync([FromBody] HotelSearchRQ searchRQ)
-        {           
-            var hotels = await _hotelService.SearchHotelsAsync(searchRQ);
-            return Ok(hotels);
+        {
+            HotelSearchRS hotelSearchRS=null;
+            try
+            {
+                hotelSearchRS = await _hotelService.SearchHotelsAsync(searchRQ);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }            
+            return Ok(hotelSearchRS);
         }
 
         [HttpPost("roomsearch")]
         public async Task<IActionResult> SearchRoomsAsync([FromBody] RoomSearchRQ roomRQ)
-        {            
-            var rooms = await _hotelService.RoomSearchAsync(roomRQ);
-            return Ok(rooms);
+        {
+            RoomSearchRS roomSearchRS = null;
+            roomSearchRS=await _hotelService.RoomSearchAsync(roomRQ);
+            try
+            {
+                roomSearchRS = await _hotelService.RoomSearchAsync(roomRQ);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+            return Ok(roomSearchRS);
         }
 
         [HttpPost("price")]
         public async Task<IActionResult> SearchRoomPriceAsync([FromBody] RoomPriceSearchRQ priceRQ)
         {
-            var priceRS = await _hotelService.RoomPriceSearchAsync(priceRQ);
-            return Ok(priceRS);
+            RoomPriceSearchRS roomPriceSearchRS = null;           
+            try
+            {
+                roomPriceSearchRS = await _hotelService.RoomPriceSearchAsync(priceRQ);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+            return Ok(roomPriceSearchRS);     
         }
         [HttpPost("book")]
         public async Task<IActionResult> BookRoomAsync([FromBody] RoomBookRQ roomBookRQ)
         {
-            var response = await _hotelService.BookRoomAsync(roomBookRQ);
-            return Ok(response);
+
+            RoomBookRS roomBookRS = null;
+            try
+            {
+                roomBookRS = await _hotelService.BookRoomAsync(roomBookRQ);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500);
+            }            
+            return Ok(roomBookRS);
         }
     }
 }
